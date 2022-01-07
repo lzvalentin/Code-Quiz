74a4efab9c77e6eb569e2ffbc884769c3a6fc941
Lorena Zuniga <lzuniga1315@gmail.com>
2022-01-06 18:43:40 -0800

    fixed buttons on js, added timer

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Code-Quiz
+
+Creating a little quiz game. This game is timed and each time you have a wrong answer time is deducted. 
--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,107 @@
-var startBtn = document.getElementById('start-btn')
-var questionContainterEl =document.getElementById('question')
-let shuffleQuestions
+var startBtn = document.getElementById('start')
+var questionContainterEl = document.getElementById('questionContainer')
+//let shuffleQuestions
 var questionEl = document.getElementById('question')
 var answerBtnEl = document.getElementById('answ-button')
+var controls = document.getElementById('controls');
+var timer;
+var time = 120;
+var countdown = document.getElementById('countdown');
+var index = 0;
 
-// click button
-startBtn.addEventListener('click', startGame)
+var questions = [
+    {
+        question: "What are Pseudo elements?",
+        options: [
+            "Add special effects to selectors",
+            "Apply styles in HTML mark-up",
+            "None of the above",
+            "All of the above"
+        ],
+        answer: "None of the above"
+
+    },
+    {
+        question: "What is 1+1?",
+        options: [
+            "1",
+            "2",
+            "3",
+            "4"
+        ],
+        answer: "2"
+
+    }
+]
+
 
 // show game
-function startGame(){
-startBtn.classList.add('hide')
-shuffleQuestions = questions.sort(Math.random()-.5)
-questionContainterEl.classList.remove('hide')
-nextQuestion()
+function startGame() {
+
+    //when the game starts we need to hide the controls container and display the question container
+    // we need to start our timer and display the time on the page
+
+    controls.setAttribute('class', 'hide');
+    questionContainterEl.removeAttribute('class');
+
+    timer = setInterval(function () {
+        time--;
+
+        countdown.textContent = time
+        if (time <= 0) {
+
+        }
+
+    }, 1000)
+
+    countdown.textContent = time;
+
+    showQuestion()
 }
 
-function nextQuestion(){
-showQuestion(shuffleQuestions)
- }
+function showQuestion() {
+    var currentQuestion = questions[index];
 
-function showQuestion(questions){
-    questionEl.innertext= question.question
+    questionEl.textContent = currentQuestion.question
+
+    answerBtnEl.innerHTML = "";
+    
+    currentQuestion.options.forEach(function (choice) {
+        var choiceBtn = document.createElement('button');
+        choiceBtn.setAttribute('value', choice);
+
+        choiceBtn.textContent = choice;
+
+        choiceBtn.onclick = buttonClick;
+
+        answerBtnEl.append(choiceBtn)
+    })
+
 }
 
+function buttonClick() {
+    console.log(this.value)
+
+    if (this.value !== questions[index].answer) {
+        time -= 2
+
+        countdown.textContent = time;
+    }
+
+    index++
+
+    if (index === questions.length) {
+        alert('game over')
+    } else {
+        showQuestion()
+    }
+
+}
 
 // function answer(){
 
 // }
 
-var questions =[
-    {
-    question: "What are Pseudo elements?",
-     answers: [ 
-         {text: "Add special effects to selectors", correct: false},
-         {text: "Apply styles in HTML mark-up", correct: false},
-         {text: "None of the above", correct: false},
-         {text: "All of the above", correct: true}
-     ]
 
-    }
-]
+// click button
+startBtn.addEventListener('click', startGame)
