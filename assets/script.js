3e013132c074220101324c76cc2f96ebeecefc47
Lorena <lzuniga1315@gmail.com>
2022-03-18 13:44:53 -0700

    finished product

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 
 var timerID;
 var timeLeft = 75;
-var timerEl=
 var timerEl = document.getElementById("timer");
 var startButton = document.getElementById("start-btn");
 var nextButton = document.getElementById("next-btn");
@@ -25,67 +24,271 @@ var shuffledQuestions, currentQuestionIndex;
 var questions = [
     {
         question: "What does CSS stand for?",
-        options: [
-            "Crazy Street Style",
-            "Cats Serpents & Snails",
-            "Code Schedule Site",
-            "Cascading Style Sheets"
+        answers: [
+         {text: "Crazy Street Style", correct: false}   ,
+         {text:  "Cats Serpents & Snails",correct: false},
+         {text:  "Code Schedule Site",correct: false},
+         {text:  "Cascading Style Sheets", correct: true}
         ],
-        answer: "Cascading Style Sheets"
+        
 
     },
     {
         question: "What is 1+1?",
-        options: [
-            "1",
-            "2",
-            "3",
-            "4"
+        answers: [
+            {text:  "1", correct: false},
+            {text:  "2",correct:true},
+            {text: "3",correct:false},
+            {text: "4",correct: false}
         ],
-        answer: "2"
-
+        
     },
     {
         question: "Are Java and JavaScript the same?",
-        options: [
-            "yes",
-            "No",
-            "Maybe",
-            "None of the above"
+        answers: [
+            {text: "yes",correct: false},
+            {text:  "No",correct: true},
+            {text: "Maybe",correct:false},
+            {text: "None of the above",correct:false}
         ],
-        answer: "No"
     },
     {
         question: "How do programmers enjoy life?",
-        options: [
-            "Seeing their code work",
-            "laughing at the comma they missed",
-            "All of the Above",
-            "None of the Above"
+        answers: [
+            {text:  "Seeing their code work", correct:false},
+            {text:  "laughing at the comma they missed",correct:false},
+            {text:  "All of the Above",correct: true},
+            {text:  "None of the Above",correct:false}
         ],
-        answer: "All the Above"
     },
     {
         question: "A loop that never ends is referred to as a(n)_________.",
-        options: [
-            "While Loop",
-            "Infinite Loop",
-            "Recursive Loop",
-            "for loop"
+        answers: [
+            {text:  "While Loop", correct:false},
+            {text:  "Infinite Loop", correct: true},
+            {text: "Recursive Loop", correct:false},
+            {text: "for loop", correct:false}
         ],
         answer: "Infinite Loop"
     },
 
     {
         question: "_______ is the process of finding errors and fixing them within a program.",
-        options: [
-            "Scanning",
-            "Executing",
-            "Compiling",
-            "Debugging"
+        answers: [
+            {text: "Scanning", correct:false},
+            {text: "Executing", correct:false},
+            {text: "Compiling", correct:false},
+            {text: "Debugging", correct:true}
         ],
         answer: "Debugging"
+    },
+    { 
+        question: "How do you write 'Hello World' in an alert box?", 
+        answers: [
+            { text: "msg('Hello World')", correct: false },
+            { text: "alert('Hello World')" , correct: true },
+            { text: "prompt('Hello World')", correct: false },
+            { text: "alertBox('Hello World')", correct: false }
+        ]
     },
 ]
 
 
+// start btn
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+});
+
+// timer
+function timeTick(){
+    timeLeft--;
+    timerEl.textContent= "Time" + timeLeft;
+    if(timeLeft <= 0){
+        saveScore();
+    }
+}
+
+// startgame
+function startGame(){
+    timerID= setInterval(timeTick, 1000);
+    startContainerEl.classList.add("hide");
+    shuffledQuestions= questions.sort(() => Math.random()-.5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove("hide")
+
+    timeTick();
+    setNextQuestion();
+};
+
+function setNextQuestion(){
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+};
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
+};
+function resetState() {
+    //clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    checkAnswerEl.classList.add("hide")
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild
+            (answerButtonsEl.firstChild)
+    }
+};
+function selectAnswer(e) {
+    var selectedButton = e.target;
+    //console.dir(selectedButton);
+    var correct = selectedButton.dataset.correct;
+    checkAnswerEl.classList.remove("hide")
+    // Check if the answer correct or wrong then show text
+    if (correct) {
+        checkAnswerEl.innerHTML = "You got it right!";
+    } else {
+        checkAnswerEl.innerHTML = "Sorry that was not the correct answer.";
+        if (timeLeft <= 10) {
+            timeLeft = 0;
+        } else {
+            // If the aswer is wrong, deduct time by 10
+            timeLeft -= 10;
+        }
+    }
+
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+        checkAnswerEl.classList.remove("hide")
+    } else {
+        startButton.classList.remove("hide")
+        saveScore();
+    }
+};
+
+
+// Check and show the correct answer by set the buttons colors
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+};
+
+
+// Remove all the classes
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong");
+};
+
+
+// Save scores
+function saveScore() {
+    clearInterval(timerID);
+    timerEl.textContent = "Time: " + timeLeft;
+    setTimeout(function () {
+        //localStorage.setItem("scores", JSON.stringify(scores));
+        questionContainerEl.classList.add("hide");
+        document.getElementById("score-container").classList.remove("hide");
+        document.getElementById("your-score").textContent = "Your final score is " + timeLeft;
+
+    }, 2000)
+};
+
+
+var loadScores = function () {
+    // Get score from local storage
+
+    if (!savedScores) {
+        return false;
+    }
+
+    // Convert scores from stringfield format into array
+    savedScores = JSON.parse(savedScores);
+    var initials = document.querySelector("#initials-field").value;
+    var newScore = {
+        score: timeLeft,
+        initials: initials
+    }
+    savedScores.push(newScore);
+    console.log(savedScores)
+
+    savedScores.forEach(score => {
+        initialsField.innerText = score.initials
+        scoreField.innerText = score.score
+    })
+};
+
+
+// Show high scores
+function showHighScores(initials) {
+    document.getElementById("highscores").classList.remove("hide")
+    document.getElementById("score-container").classList.add("hide");
+    startContainerEl.classList.add("hide");
+    questionContainerEl.classList.add("hide");
+    if (typeof initials == "string") {
+        var score = {
+            initials, timeLeft
+        }
+        scores.push(score)
+    }
+
+    var highScoreEl = document.getElementById("highscore");
+    highScoreEl.innerHTML = "";
+    //console.log(scores)
+    for (i = 0; i < scores.length; i++) {
+        var div1 = document.createElement("div");
+        div1.setAttribute("class", "name-div");
+        div1.innerText = scores[i].initials;
+        var div2 = document.createElement("div");
+        div2.setAttribute("class", "score-div");
+        div2.innerText = scores[i].timeLeft;
+
+        highScoreEl.appendChild(div1);
+        highScoreEl.appendChild(div2);
+    }
+
+    localStorage.setItem("scores", JSON.stringify(scores));
+
+};
+
+
+// View high scores link
+viewHighScores.addEventListener("click", showHighScores);
+
+
+submitButton.addEventListener("click", function (event) {
+    event.preventDefault()
+    var initials = document.querySelector("#initials-field").value;
+    showHighScores(initials);
+});
+
+
+// Restart or reload the page
+restartButton.addEventListener("click", function () {
+    window.location.reload();
+});
+
+
+// Clear localStorage items 
+clearScoreButton.addEventListener("click", function () {
+    localStorage.clear();
+    document.getElementById("highscore").innerHTML = "";
+});
